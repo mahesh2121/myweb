2ad324984c81ef82eef39ffa280a588110d338a1
mahesh2121git coffig --global user.email maheshgaikwad128@gmail.com <maheshgaikwad128@gmail.com>
2022-02-23 11:42:55 +0530

    Test case corrected

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,7 +42,7 @@ public class TestGreeter {
   }
   
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMsg() {
     String someone = "World, Thanks you very much";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
